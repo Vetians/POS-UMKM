e693b234b3dc6895de3fbe0c4a9580e154dc5673
Vetians <143514462+Vetians@users.noreply.github.com>
2026-08-16 15:01:39 +0700

    pembetulan http dan https di modal tambah produk

--- resources/views/produk/index.blade.php
+++ resources/views/produk/index.blade.php
@@ -126,7 +126,7 @@ let actionUrl = '';
 let _hapusId = null, _hapusBtn = null; // Variabel penampung elemen global
 
 function bukaModalTambah() {
-  actionUrl = '/data-produk/{{ $kategori->id }}';
+  actionUrl = '{{ route("produk.store", $kategori, false) }}';
   document.getElementById('modalProdukTitle').textContent = 'Tambahkan Produk';
   document.getElementById('produkMethod').value = 'POST';
   document.getElementById('fKode').value = '';
